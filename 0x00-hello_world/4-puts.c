538ad5d76f5ce4a6c0429de7ae90bdb2db2b3946
BNdiema <bryantndiema@gmail.com>
2023-02-16 17:50:42 -0800

    Print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-/* Entry point*/
-int main (void)
+/* Main Entry point*/
+int main(void)
 {
-
    puts("\"Programming is like building a multilingual puzzle");
-
    return (0);
 }
