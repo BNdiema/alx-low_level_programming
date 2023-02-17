d606dc2f863f05371e720f07034993af9c05b58b
BNdiema <bryantndiema@gmail.com>
2023-02-16 19:00:24 -0800

    Size of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) /* Entry point */
+{
+  printf("size of a float: %d byte(x)\n", size(float));
+  printf("size of an int: %d bytes(x)\n", size(int));
+  printf("size of a char: %d bytes(x)\n", size(char))
+    return(0);
+}  
