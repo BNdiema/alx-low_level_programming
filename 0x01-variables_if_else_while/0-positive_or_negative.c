84292f58226f3d336c565c45084ccd562e9b8c72
BNdiema <bryantndiema@gmail.com>
2023-02-19 15:32:23 -0800

    Posivive, 0, and negative number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,24 +3,23 @@
 
 int main(void)/* Main entry point */
 {
-  int n;      /* Variable declaration */
-
-  scanf("%d", &n); /* The imput is put in n after a number is typed */
+int n;      /* Variable declaration */
+n=0;
+scanf("%d", &n); /* The imput is put in n after a number is typed */
   if (n < 0)
-    {
-      printf("%d is greater than 0: is positive"); /* Prints out the number enterd + the statement */
-    }
-  scanf("%d", &n);
-  if(n == 0)
-    {
-      printf("%d is 0: is zero");
-    }
-  scanf("%d", &n);
-  if(n > 0);
-    {
-      printf("%d is less than 0: is negative\n");
-    }
-    return 0;
+{
+printf("%d is greater than 0: is positive"); /* Prints number enterd plus statement */
+}
+scanf("%d", &n);
+if (n == 0)
+{
+printf("%d is 0: is zero");
+}
+scanf("%d", &n);
+if (n > 0);
+{
+printf("%d is less than 0: is negative\n");
+}
+return 0;
 }  
-	
-  
+	  
