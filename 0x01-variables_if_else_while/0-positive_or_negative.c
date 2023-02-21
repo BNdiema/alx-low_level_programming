aa5abb16605bbdf9256104489976e682bbadd9c1
BNdiema <bryantndiema@gmail.com>
2023-02-21 04:20:41 -0800

    fix 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,19 +5,17 @@ int main(void)
 {
 int n;
 n = 0;
-scanf("%d", &n)
 if (n < 0)
 {
 printf("%d is greater than 0: is positive");
 }
-scanf("%d", &n);
 else if (n == 0)
 {
 printf("%d is 0: is zero");
 }
-scanf("%d", &n);
 if (n > 0)
 {
 printf("%d is less than 0: is negative\n");
 }
 return (0);
+}
