7958ed547919c8d0c578ebb2eaf5b5ef021d6ecd
BNdiema <bryantndiema@gmail.com>
2023-02-21 05:01:31 -0800

    Fix2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,13 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n < 0)
 {
-printf("%d is greater than 0: is positive\n", n);
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d is 0: is zero\n", n);
+printf("% is zero\n", n);
 }
 else
 {
-printf("%d is less than 0: is negative\n", n);
-}
-return (0);
+  printf("%d is negative\n", n;
 }
