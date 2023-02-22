4611a604e344202ef565b4c5d8f0ff703f74bcb9
BNdiema <bryantndiema@gmail.com>
2023-02-21 16:27:37 -0800

    fixxing betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-printf("size of a float: %d byte(x)\n", sizeof(float));
-printf("size of an int: %d bytes(x)\n", sizeof(int));
-printf("size of a char: %d bytes(x)\n", sizeof(char))
+printf("size of a float: %zu byte(x)\n", sizeof(float));
+printf("size of an int: %zu bytes(x)\n", sizeof(int));
+printf("size of a char: %zu bytes(x)\n", sizeof(char))
 return (0);
 }
