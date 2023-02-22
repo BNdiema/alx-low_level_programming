5d0d43e132590bef2497bd9ba9a6858c1fddf87e
BNdiema <bryantndiema@gmail.com>
2023-02-21 16:51:30 -0800

    Addining statments

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ printf("size of a float: %zu byte(s)\n", sizeof(float));
 printf("size of an int: %zu bytes(s)\n", sizeof(int));
 printf("size of a char: %zu bytes(s)\n", sizeof(char));
 printf("size of a long int: %zu bytes(s)\n", sizeof(long int));
-printf("size of long long int : %zu bytes(s)\n", sizeof(long long int));
+printf("size of a long long int: %zu bytes(s)\n", sizeof(long long int));
 return (0);
 }
