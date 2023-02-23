7ee0dfc211aed11aa936ecfd3dc0900c6a0c5e86
BNdiema <bryantndiema@gmail.com>
2023-02-22 16:14:21 -0800

    Aphabet char

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+int c;
+if (c >= 'a'; &&c <= 'z')
+{
+_putchar(1);
+}
+else if (c >= 'A'; &&c <= 'Z')
+_putchar(1);
+else
+{
+_putchar(0);
+}
+return (0);
+}
