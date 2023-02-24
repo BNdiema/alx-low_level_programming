e4eeadcdc8e80ac74e7cc883b3a3237dc92a07fb
BNdiema <bryantndiema@gmail.com>
2023-02-24 15:24:20 -0800

    Printing no 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print no 0 to 9
+ *
+ * Return always 0
+ */
+
+void print_numbers(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+
+putchar(n);
+putchar('\n');
+}
