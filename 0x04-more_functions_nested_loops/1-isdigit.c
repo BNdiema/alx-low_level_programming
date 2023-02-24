ae805e85d3ddd4b3c650390efe7faa5180abd41d
BNdiema <bryantndiema@gmail.com>
2023-02-23 17:59:36 -0800

    Fixer AICC

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *Return: always 0
+ */
+
+int _isdigit(int c);
+{
+for (c >= 48; c <= 57)
+return (1);
+else
+return (0)
+}
