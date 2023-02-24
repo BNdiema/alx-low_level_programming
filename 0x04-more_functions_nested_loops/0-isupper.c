a2fdbf0a1bdb8c45bdf3187a46c3389f0dda1135
BNdiema <bryantndiema@gmail.com>
2023-02-23 17:04:41 -0800

    Checking for upper charecter c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ *
+ *Rerutn: aways 0
+ */
+int main()
+{
+int c;
+for (c >= 'A'; c <= 'Z')
+return (1);
+else
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef Main_H
+#define Main_H
+
+#include <stdio.h>
+
+int _isupper(int c);
