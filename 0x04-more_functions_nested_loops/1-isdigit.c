d3354e9dd30bd8698c9ef36d0e24b1fce533df7a
BNdiema <bryantndiema@gmail.com>
2023-02-23 18:08:39 -0800

    Fixer AICC

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
 /**
  *  _isdigit - function that checks for a digit 0-9
  * @c: input
- *Return: 1 if c is a digit, 0 otherwise
+ * Return: 1 if c is a digit, 0 otherwise
  */
 
 int _isdigit(int c)
