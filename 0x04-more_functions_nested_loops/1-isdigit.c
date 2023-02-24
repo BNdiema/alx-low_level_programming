b47ce20a50e8a5f34542bcc933ce7911ef4824c9
BNdiema <bryantndiema@gmail.com>
2023-02-23 18:12:07 -0800

    Fixer AICC

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  *  _isdigit - function that checks for a digit 0-9
  * @c: input
@@ -11,5 +12,5 @@ int _isdigit(int c)
 for ((c >= 48) && (c <= 57))
 return (1);
 else
-return (0)
+return (0);
 }
