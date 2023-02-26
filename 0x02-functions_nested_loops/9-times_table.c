8e60143261c58aca1ba5b657afa7babdf58fcd40
BNdiema <bryantndiema@gmail.com>
2023-02-26 11:37:55 -0800

    fixed .c file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,20 +10,20 @@ void times_table(void)
 int O;
 int num;
 int table;
-for (O = 0; O < 10; O++)
+for (O = 0; O <= 9; O++)
 {
 _putchar(0);
-for (num = 1; num < 10; num++)
+for (num = 1; num <= 9; num++)
 {
 _putchar(',');
 _putchar(' ');
-table = 0 * num;
-if (table < 10)
+table = O * num;
+if (table <= 9)
 _putchar (' ');
 else
 _putchar((table / 10) + '0');
 _putchar((table % 10) + '0');
 }
-_putchar(table);
+_putchar('\n');
 }
 }
