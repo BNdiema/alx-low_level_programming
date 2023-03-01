6a61ad1c290a951859864988e6ac8a6d23f303a1
BNdiema <bryantndiema@gmail.com>
2023-02-28 16:41:57 -0800

    Reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string
+ * Return: reversed string *s
+ */
+
+void rev_string(char *s)
+{
+int l = 0;
+while (s[l] != '\0')
+{
+l++;
+}
+while (l > 0)
+{
+_putchar(s[--l]);
+}
+_putchar('\n');
+}
