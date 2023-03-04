e1dd61aeaf78d5b4de198b764ea5d270007ee701
BNdiema <bryantndiema@gmail.com>
2023-03-03 17:53:24 -0800

    capitalize all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string -capitalize each word
+ * @w: pointer to a string
+ * Return: w
+ */
+
+char *cap_string(char *w)
+{
+int capp = 1;
+char *l = w;
+while (*l != '\0')
+{
+if (capp && *l  >= 'a' && *l <= 'z')
+{
+*l = *l - 'a' + 'A';
+}
+if (',' || ';' || '.' || '!' || '?' || '"' || '(' || ')' || '{' || '}')
+{
+capp = 1;
+}
+else
+{
+capp = (0);
+}
+*l++;
+}
+return (w);
+}
