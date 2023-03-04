31cdadd20a1a512d841bfce4664ae3f7fe25d87f
BNdiema <bryantndiema@gmail.com>
2023-03-03 16:17:57 -0800

    Copies string same as function strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,9 +11,20 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
+int s = 0;
+while (src[s])
+{
+s++;
+}
 while (i < n && src[i] != '\0')
 {
 dest[i] = src[i];
 i++;
 }
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
 }
