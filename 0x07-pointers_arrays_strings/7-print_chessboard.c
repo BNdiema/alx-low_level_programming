de7647fde49e8fb8274f92d721535fef87753d3d
BNdiema <bryantndiema@gmail.com>
2023-03-06 18:23:07 -0800

    fixed .c file

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,9 +9,11 @@
 
 void print_chessboard(char (*a)[8])
 {
-for (int row = 0; row < 8; row++)
+int row;
+int col;
+for (row = 0; row < 8; row++)
 {
-for (int col = 0; col < 8; col++)
+for (col = 0; col < 8; col++)
 {
 _putchar(a[row][col]);
 _putchar(' ');
