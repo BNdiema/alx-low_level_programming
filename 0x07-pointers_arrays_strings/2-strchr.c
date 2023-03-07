47c1c109a53b450a4dba764685b322b215ec392d
BNdiema <bryantndiema@gmail.com>
2023-03-06 16:11:54 -0800

    Fixed .c file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: Pointer to a string
+ * @c: Character to be located
+ * Return: a pointer to character c in string @s or NUll
+ */
+
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+return (s);
+s++;
+}
+return (NULL);
+}
