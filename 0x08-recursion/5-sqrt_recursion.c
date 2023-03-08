e66b51cfd3847f4056e8765be4e7ff8f6e25935f
BNdiema <bryantndiema@gmail.com>
2023-03-08 14:55:23 -0800

    Fixed .c file

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,18 +8,11 @@
 
 int _sqrt_recursion(int n)
 {
-int s = 1;
-int i = 1;
 if (n < 0)
 {
 return (-1);
 if (n == 1 || n == 0)
 return (n);
 }
-if (s <= n)
-{
-i++;
-s = (i << 1) + 1;
-}
-return (i - 1);
-}
+else
+return (n * _sqrt_recursion(n + 1));
