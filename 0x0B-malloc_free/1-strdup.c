40b72854ffc694f863bf379363cc6b173a8c8f7c
BNdiema <bryantndiema@gmail.com>
2023-03-15 10:30:26 -0700

    fixed.c file

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,12 +14,12 @@ char *new;
 unsigned int l = 0;
 if (str == 0)
 return (0);
-while (str[l] != '\0')
+while (str[l] != 0)
 l++;
 l++;
 new = malloc(l * (sizeof(char)));
-for (l = 0; new[l] != '\0'; l++)
+for (l = 0; new[l] != 0; l++)
 str[l] = new[l];
-new[l] = '\0';
+new[l] = 0;
 return (new);
 }
