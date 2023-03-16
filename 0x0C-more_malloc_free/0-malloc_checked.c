32c3342dd6e48299d2f9fd7dc4c6f73e90a16dc0
BNdiema <bryantndiema@gmail.com>
2023-03-16 14:29:01 -0700

    Fixed .c file

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
+#include <limits.h>
 /**
  * malloc_checked - allocates memory using malloc.
- * @b: unsigned integer
+ * @b: size
+ * 
  * Return: Always 0
  */
 
