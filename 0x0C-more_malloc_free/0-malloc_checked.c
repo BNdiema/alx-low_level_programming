b3a91fe78f8119faafcf2aa2f7faa8649b0534a0
BNdiema <bryantndiema@gmail.com>
2023-03-16 14:23:55 -0700

    Fixed .c file

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,15 +3,15 @@
 #include "main.h"
 
 /**
- * malloc_checked  - allocates memory using malloc.
- * @b: size of data type
- * Return: 0
+ * malloc_checked - allocates memory using malloc.
+ * @b: unsigned integer
+ * Return: Always 0
  */
 
 void *malloc_checked(unsigned int b)
 {
 void *ptr;
-ptr =  malloc(b);
+ptr = malloc(b);
 if (ptr == NULL)
 exit(98);
 return (ptr);
