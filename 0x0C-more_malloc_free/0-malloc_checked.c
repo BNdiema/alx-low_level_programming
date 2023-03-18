7fb0a8d02578686620f9de7ba8c822f24f7df576
BNdiema <bryantndiema@gmail.com>
2023-03-18 03:45:16 -0700

    Fixed .c file

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,21 +1,19 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 #include <limits.h>
 
 /**
- * *malloc_checked - allocates memory using malloc.
- * @b: size
- *
- * Return: Pointer to memory
- */
-
+  * malloc_checked - Allocates memory
+  * @b: the size to allocate
+  *
+  * Return: pointer to the allocated memory
+  */
 void *malloc_checked(unsigned int b)
 {
-void *ptr = malloc(b);
+void *ptr;
+ptr = malloc(b);
 if (ptr == NULL)
-{
 exit(98);
-}
 return (ptr);
 }
---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to previoous memory
+ * @old_size: size of ptr
+ * @new_size: size of new block
+ * Return: pointer to result of string
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+{
+  void *newptr;
+ if
+}
