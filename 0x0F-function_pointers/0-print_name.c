be99e52f91c31f2a8cc45f40e2c29ad298c25719
BNdiema <bryantndiema@gmail.com>
2023-03-23 18:59:02 -0700

    Fixed .c file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -15,5 +15,8 @@ int i = 0;
 if (name == NULL || f == NULL)
 return;
 while (name[i] != '\0')
+{
 f(&name[i]);
+i++;
+}
 }
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - unction that searches for an integer.
+ * @array: pointer to array
+ * @size: number of elements in the array
+ * @cmp: Function pointer
+ * Return: index of first element
+ */
