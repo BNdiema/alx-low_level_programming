f6dc6d5f67ec7961835cb2558a87e3a30d0ab689
BNdiema <bryantndiema@gmail.com>
2023-04-03 14:52:43 -0700

    fixed .h line 13 white spaces and 22 space after } requred

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -10,16 +10,16 @@
  * @str: (malloc'ed string)
  * @len: lenth of the string
  * @next:points to next node
- * 
+ *
  *Description : siingly linked list node structure
  */
 
 typedef struct list_s
 {
-  char *str;
-  unsigned int len;
-  struct list_s *next;
-}list_t;
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 
