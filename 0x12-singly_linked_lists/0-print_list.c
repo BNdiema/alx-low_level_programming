ea4611d98f3ce643c3dfc1a0583f7ba8a19a4233
BNdiema <bryantndiema@gmail.com>
2023-04-03 15:41:47 -0700

    Fixed .c file new line

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,9 +12,8 @@ size_t print_list(const list_t *h)
 size_t i;
 i = 0;
 if ((*h).str == NULL)
-{
-printf("[0] (nil)");
-}
+printf("[0] (nil)\n");
+
 while (h != NULL)
 {
 printf("[%d] %s\n", h->len, h->str);
