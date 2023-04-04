9b5d8f1dd82a4532d2b892217665720cd0222076
BNdiema <bryantndiema@gmail.com>
2023-04-04 01:25:53 -0700

    removed loop

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,24 +1,38 @@
 #include "main.h"
 
 /**
- * is_prime_number - checks if n is prime
+ * prime_check - checks if number is prime
  *
- * @n: integer
+ * @f: factor check
+ * @p: integer
  *
  * Return: 1 or 0
  */
 
+int prime_check(int f, int p)
+{
+if (p < 2)
+{
+return (0);
+}
+if (p % f == 0)
+return (0);
+else if (f > p / 2)
+return (1);
+else
+return (prime_check(f + 1, p));
+}
+
+
+/**
+ * is_prime_number - states if number is prime
+ * @n: integer to check
+ * Return: 1 if prime, 0 if not
+ */
+
 int is_prime_number(int n)
 {
-int i = 0;
-if (n <= 1)
-{
-return (0);
-}
-for (i = 2; i < n; i++)
-{
-if (n % i == 0)
-return (0);
-}
+if (n == 2)
 return (1);
+return (prime_check(2, n));
 }
